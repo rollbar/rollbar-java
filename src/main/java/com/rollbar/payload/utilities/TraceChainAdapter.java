8558581ce0ddd16285b4e3375501cafa7f9021c3
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-11-30 15:13:35 -0500

    Document the TraceChainAdapter

--- src/main/java/com/rollbar/payload/utilities/TraceChainAdapter.java
+++ src/main/java/com/rollbar/payload/utilities/TraceChainAdapter.java
@@ -7,7 +7,17 @@ import com.rollbar.payload.data.body.TraceChain;
 
 import java.lang.reflect.Type;
 
+/**
+ * The Adapter for Trace Chains. Serializes the whole thing as an array.
+ */
 public class TraceChainAdapter implements JsonSerializer<TraceChain> {
+    /**
+     * Serializes a TraceChain as an array of traces.
+     * @param traceChain the trace chain
+     * @param type the runtime type of the trace chain
+     * @param jsonSerializationContext the context doing the serializing
+     * @return the JsonElement representing the TraceChain
+     */
     public JsonElement serialize(TraceChain traceChain, Type type, JsonSerializationContext jsonSerializationContext) {
         return jsonSerializationContext.serialize(traceChain.traces());
     }
