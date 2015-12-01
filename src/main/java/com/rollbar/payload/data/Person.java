5407cf5ae330d110861d93c54d94fb48b1bfccd6
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-12-01 15:08:16 -0500

    FIx Person checked exceptoins, and document

--- src/main/java/com/rollbar/payload/data/Person.java
+++ src/main/java/com/rollbar/payload/data/Person.java
@@ -4,11 +4,19 @@ import com.rollbar.payload.utilities.ArgumentNullException;
 import com.rollbar.payload.utilities.InvalidLengthException;
 import com.rollbar.payload.utilities.Validate;
 
+/**
+ * Represents the user affected by an error
+ */
 public class Person {
     private final String id;
     private final String username;
     private final String email;
 
+    /**
+     * Constructor
+     * @param id the affected user's id
+     * @throws ArgumentNullException if {@code id} is null
+     */
     public Person(String id) throws ArgumentNullException {
         Validate.isNotNullOrWhitespace(id, "id");
         this.id = id;
@@ -16,6 +24,14 @@ public class Person {
         email = null;
     }
 
+    /**
+     * Constructor
+     * @param id the affected user's id
+     * @param username the affected user's username
+     * @param email the affected user's email address
+     * @throws InvalidLengthException if {@code username} or {@code email} are longer than 255 characters
+     * @throws ArgumentNullException if {@id} is null
+     */
     public Person(String id, String username, String email) throws InvalidLengthException, ArgumentNullException {
         Validate.isNotNullOrWhitespace(id, "id");
         this.id = id;
@@ -29,41 +45,54 @@ public class Person {
         this.email = email;
     }
 
+    /**
+     * @return the affected user's id
+     */
     public String id() {
         return this.id;
     }
 
+    /**
+     * Set the id on a copy of this Person
+     * @param id the new id
+     * @return a copy of this person with the new id
+     * @throws ArgumentNullException if {@code id} is null
+     */
     public Person id(String id) throws ArgumentNullException {
-        try {
-            return new Person(id, username, email);
-        } catch (InvalidLengthException e) {
-            throw new IllegalStateException("username isn't changing, so this error shouldn't occur");
-        }
+        return new Person(id, username, email);
     }
 
+    /**
+     * @return the affected user's username
+     */
     public String username() {
         return this.username;
     }
 
+    /**
+     * Set the username on a copy of this Person
+     * @param username the new username
+     * @return a copy of this person with the new username
+     * @throws InvalidLengthException if {@code username} is longer than 255 characters
+     */
     public Person username(String username) throws InvalidLengthException {
-        try {
-            return new Person(id, username, email);
-        } catch (ArgumentNullException e) {
-            throw new IllegalStateException("id can't be null");
-        }
+        return new Person(id, username, email);
     }
 
+    /**
+     * @return the affected user's email
+     */
     public String email() {
         return this.email;
     }
 
-    public Person email(String email) {
-        try {
-            return new Person(id, username, email);
-        } catch (ArgumentNullException e) {
-            throw new IllegalStateException("id can't be null");
-        } catch (InvalidLengthException e) {
-            throw new IllegalStateException("username can't be the wrong length");
-        }
+    /**
+     * Set the email on a copy of this Person
+     * @param email
+     * @return a copy of this person with the new email
+     * @throws InvalidLengthException if {@code email} is longer than 255 characters
+     */
+    public Person email(String email) throws InvalidLengthException {
+        return new Person(id, username, email);
     }
 }
