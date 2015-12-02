a80176f08401d7c19db6abd38d4e24ef2242e18a
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-12-02 13:30:57 -0500

    Document Frame

--- src/main/java/com/rollbar/payload/data/body/Frame.java
+++ src/main/java/com/rollbar/payload/data/body/Frame.java
@@ -8,7 +8,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
+/**
+ * Represents a single frame from a stack trace
+ */
 public class Frame {
+    /**
+     * Get an array of frames from an error
+     * @param error the error
+     * @return the frames representing the error's stack trace
+     * @throws ArgumentNullException if error is null
+     */
     public static Frame[] fromThrowable(Throwable error) throws ArgumentNullException {
         Validate.isNotNull(error, "error");
         StackTraceElement[] elements = error.getStackTrace();
@@ -20,6 +29,12 @@ public class Frame {
         return result.toArray(new Frame[result.size()]);
     }
 
+    /**
+     * Get a Frame from a StackTraceElement
+     * @param stackTraceElement the StackTraceElement (a.k.a.: stack frame)
+     * @return the Frame representing the StackTraceElement
+     * @throws ArgumentNullException if stackTraceElement is null
+     */
     public static Frame fromStackTraceElement(StackTraceElement stackTraceElement) throws ArgumentNullException {
         String filename = stackTraceElement.getClassName() + ".java";
         Integer lineNumber = stackTraceElement.getLineNumber();
@@ -40,10 +55,27 @@ public class Frame {
     @SerializedName("kwargs")
     private final HashMap<String, Object> keywordArgs;
 
+    /**
+     * Constructor
+     * @param filename the name of the file in which the error occurred
+     * @throws ArgumentNullException if filename is null
+     */
     public Frame(String filename) throws ArgumentNullException {
         this(filename, null, null, null, null, null, null, null);
     }
 
+    /**
+     * Constructor
+     * @param filename the name of the file in which the error occurred
+     * @param lineNumber the line number on which the error occurred
+     * @param columnNumber the column number (if available in your language) on which the error occurred
+     * @param method the method in which the error occurred
+     * @param code the line of code that triggered the error
+     * @param context extra context around the line of code that triggered the error
+     * @param args the arguments to the method from the stack frame (if available in your language)
+     * @param keywordArgs the keyword arguments to the method from the stack frame (if available in your language)
+     * @throws ArgumentNullException if filename is null
+     */
     public Frame(String filename, Integer lineNumber, Integer columnNumber, String method, String code, CodeContext context, Object[] args, HashMap<String, Object> keywordArgs) throws ArgumentNullException {
         Validate.isNotNullOrWhitespace(filename, "filename");
         this.filename = filename;
@@ -56,18 +88,35 @@ public class Frame {
         this.keywordArgs = keywordArgs == null ? null : new HashMap<String, Object>(keywordArgs);
     }
 
+    /**
+    * @return the name of the file in which the error occurred
+    */
     public String filename() {
         return filename;
     }
 
+    /**
+    * Set filename on a copy of this Frame
+    * @param filename the name of the file in which the error occurred
+    * @return a copy of this Frame with the filename overridden
+    * @throws ArgumentNullException if filename is null
+    */
     public Frame filename(String filename) throws ArgumentNullException {
         return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
     }
 
+    /**
+    * @return the line number on which the error occurred
+    */
     public Integer lineNumber() {
         return lineNumber;
     }
 
+    /**
+    * Set lineNumber on a copy of this Frame
+    * @param lineNumber the line number on which the error occurred
+    * @return a copy of this Frame with the lineNumber overridden
+    */
     public Frame lineNumber(Integer lineNumber) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
@@ -76,10 +125,18 @@ public class Frame {
         }
     }
 
+    /**
+    * @return the column number (if available in your language) on which the error occurred
+    */
     public Integer columnNumber() {
         return columnNumber;
     }
 
+    /**
+    * Set columnNumber on a copy of this Frame
+    * @param columnNumber the column number (if available in your language) on which the error occurred
+    * @return a copy of this Frame with the columnNumber overridden
+    */
     public Frame columnNumber(Integer columnNumber) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
@@ -88,10 +145,18 @@ public class Frame {
         }
     }
 
+    /**
+    * @return the method in which the error occurred
+    */
     public String method() {
         return method;
     }
 
+    /**
+    * Set method on a copy of this Frame
+    * @param method the method in which the error occurred
+    * @return a copy of this Frame with the method overridden
+    */
     public Frame method(String method) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
@@ -100,10 +165,18 @@ public class Frame {
         }
     }
 
+    /**
+    * @return the line of code that triggered the error
+    */
     public String code() {
         return code;
     }
 
+    /**
+    * Set code on a copy of this Frame
+    * @param code the line of code that triggered the error
+    * @return a copy of this Frame with the code overridden
+    */
     public Frame code(String code) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
@@ -112,10 +185,18 @@ public class Frame {
         }
     }
 
+    /**
+    * @return extra context around the line of code that triggered the error
+    */
     public CodeContext context() {
         return context;
     }
 
+    /**
+    * Set context on a copy of this Frame
+    * @param context extra context around the line of code that triggered the error
+    * @return a copy of this Frame with the context overridden
+    */
     public Frame context(CodeContext context) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
@@ -124,10 +205,18 @@ public class Frame {
         }
     }
 
+    /**
+    * @return the arguments to the method from the stack frame (if available in your language)
+    */
     public Object[] args() {
         return args == null ? null : args.clone();
     }
 
+    /**
+    * Set args on a copy of this Frame
+    * @param args the arguments to the method from the stack frame (if available in your language)
+    * @return a copy of this Frame with the args overridden
+    */
     public Frame args(Object[] args) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
@@ -136,10 +225,18 @@ public class Frame {
         }
     }
 
+    /**
+    * @return the keyword arguments to the method from the stack frame (if available in your language)
+    */
     public HashMap<String, Object> keywordArgs() {
         return keywordArgs == null ? null : new HashMap<String, Object>(keywordArgs);
     }
 
+    /**
+    * Set keywordArgs on a copy of this Frame
+    * @param keywordArgs the keyword arguments to the method from the stack frame (if available in your language)
+    * @return a copy of this Frame with the keywordArgs overridden
+    */
     public Frame keywordArgs(HashMap<String, Object> keywordArgs) {
         try {
             return new Frame(filename, lineNumber, columnNumber, method, code, context, args, keywordArgs);
