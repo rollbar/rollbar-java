e2ddeb5925811a76720ac15ddf08fb70691a1cf2
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-12-03 12:51:08 -0500

    Document the BodyContents marker interface

--- src/main/java/com/rollbar/payload/data/body/BodyContents.java
+++ src/main/java/com/rollbar/payload/data/body/BodyContents.java
@@ -1,4 +1,8 @@
 package com.rollbar.payload.data.body;
 
+/**
+ * A marker interface for the contents of the rollbar body.
+ * Treated specially by {@link com.rollbar.payload.utilities.BodyAdapter}
+ */
 public interface BodyContents {
 }
