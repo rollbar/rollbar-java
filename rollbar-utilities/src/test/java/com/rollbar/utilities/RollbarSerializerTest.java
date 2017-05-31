1f86021112c73a0fcd2d5358421fdbb83ac4e0c2
Andrew Ledvina <wvvwwvw@gmail.com>
2017-05-30 19:39:13 -0700

    deal with the fact that hash maps are not ordered

--- rollbar-utilities/src/test/java/com/rollbar/utilities/RollbarSerializerTest.java
+++ rollbar-utilities/src/test/java/com/rollbar/utilities/RollbarSerializerTest.java
@@ -12,15 +12,13 @@ public class RollbarSerializerTest {
     public void testSerializeObject() throws Exception {
         RollbarSerializer s = new RollbarSerializer();
         Map<String, Object> obj = new HashMap<String, Object>();
-        obj.put("key1", 42);
         obj.put("a", "a \" b");
         obj.put("other", "some \\\" string");
         FakeData d = new FakeData(obj);
         String result = s.serialize(d);
-        Assert.assertEquals(
-            "should serialize object properly",
-            "{\"key1\":42,\"a\":\"a \\\" b\",\"other\":\"some \\\\\\\" string\"}",
-            result);
+        String expected1 = "{\"a\":\"a \\\" b\",\"other\":\"some \\\\\\\" string\"}";
+        String expected2 = "{\"other\":\"some \\\\\\\" string\",\"a\":\"a \\\" b\"}";
+        Assert.assertTrue(expected1.equals(result) || expected2.equals(result));
     }
 
     @Test
@@ -28,17 +26,15 @@ public class RollbarSerializerTest {
         RollbarSerializer s = new RollbarSerializer();
         Map<String, Object> obj = new HashMap<String, Object>();
         obj.put("k", "v");
-        obj.put("a", "b");
         Map<String, Object> innerObj = new HashMap<String, Object>();
         innerObj.put("x", "y");
         obj.put("inner", innerObj);
         Object[] a = {42,obj,"hello"};
         FakeData d = new FakeData(a);
         String result = s.serialize(d);
-        Assert.assertEquals(
-            "should serialize array properly",
-            "[42,{\"a\":\"b\",\"k\":\"v\",\"inner\":{\"x\":\"y\"}},\"hello\"]",
-            result);
+        String expected1 = "[42,{\"k\":\"v\",\"inner\":{\"x\":\"y\"}},\"hello\"]";
+        String expected2 = "[42,{\"inner\":{\"x\":\"y\"},\"k\":\"v\"},\"hello\"]";
+        Assert.assertTrue(expected1.equals(result) || expected2.equals(result));
     }
 
     @Test
@@ -46,16 +42,12 @@ public class RollbarSerializerTest {
         RollbarSerializer s = new RollbarSerializer(true);
         Map<String, Object> obj = new HashMap<String, Object>();
         obj.put("key1", 42);
-        obj.put("a", "a \" b");
-        obj.put("other", "some \\\" string");
         FakeData d = new FakeData(obj);
         String result = s.serialize(d);
         Assert.assertEquals(
             "should serialize object properly",
             "{\n" +
-            "  \"key1\": 42,\n" + 
-            "  \"a\": \"a \\\" b\",\n" +
-            "  \"other\": \"some \\\\\\\" string\"\n" +
+            "  \"key1\": 42\n" +
             "}",
             result);
     }
