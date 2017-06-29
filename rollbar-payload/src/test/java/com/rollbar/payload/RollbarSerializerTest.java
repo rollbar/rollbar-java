edf2aa3974d7aed087318b9b5d45dadb8cdb6bd1
aistomin <andrej.istomin.ikeen@gmail.com>
2017-06-29 02:00:24 +0200

    RollbarSerializer.serializeValue(() was adjusted: now it can serialise custom client's classes using .toString()

--- rollbar-payload/src/test/java/com/rollbar/payload/RollbarSerializerTest.java
+++ rollbar-payload/src/test/java/com/rollbar/payload/RollbarSerializerTest.java
@@ -106,6 +106,25 @@ public class RollbarSerializerTest {
         }
     }
 
+    @Test
+    public void TestCustomTypeSerialize() {
+        final CustomClass custom = new CustomClass();
+        final Message msg = new Message("customParams");
+        final Body body = new Body(msg.put("someParam", custom));
+        final Data data = new Data(environment, body);
+        final Payload payload = new Payload(accessToken, data);
+        final String json = new RollbarSerializer().serialize(payload);
+        final JsonObject parsed = (JsonObject) new JsonParser().parse(json);
+        assertEquals(
+            custom.toString(),
+            parsed
+                .getAsJsonObject("data")
+                .getAsJsonObject("body")
+                .getAsJsonObject("message")
+                .getAsJsonPrimitive("someParam").getAsString()
+        );
+    }
+
     public Throwable getError() {
         try {
             throwException();
@@ -133,6 +152,17 @@ public class RollbarSerializerTest {
             throwException();
         } catch (Exception e) {
             throw new Exception("Wrapper Exception", e);
+        }
+    }
+
+    /**
+     * Class which is used for checking custom classes serialization.
+     */
+    public class CustomClass {
+
+        @Override
+        public String toString() {
+            return "custom string representation";
         }
     }
 }
