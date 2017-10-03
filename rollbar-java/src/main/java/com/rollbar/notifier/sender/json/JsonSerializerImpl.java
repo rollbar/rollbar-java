c67a0fd19fe0eea7e8f868b95232036bdbda05a6
David Basoco <dbasoco@rollbar.com>
2017-10-03 08:50:21 +0200

    Update JsonSerializerImpl code from last version

--- rollbar-java/src/main/java/com/rollbar/notifier/sender/json/JsonSerializerImpl.java
+++ rollbar-java/src/main/java/com/rollbar/notifier/sender/json/JsonSerializerImpl.java
@@ -23,78 +23,44 @@ public class JsonSerializerImpl implements JsonSerializer {
 
   private static final Pattern UUID_PATTERN = compile("\"uuid\"\\s*:\\s*\"([^\"]*)\"");
 
+  private static final String[] REPLACEMENT_CHARS;
+
+  static {
+    REPLACEMENT_CHARS = new String[128];
+    for (int i = 0; i <= 0x1f; i++) {
+      REPLACEMENT_CHARS[i] = String.format("\\u%04x", (int) i);
+    }
+    REPLACEMENT_CHARS['"'] = "\\\"";
+    REPLACEMENT_CHARS['\\'] = "\\\\";
+    REPLACEMENT_CHARS['\t'] = "\\t";
+    REPLACEMENT_CHARS['\b'] = "\\b";
+    REPLACEMENT_CHARS['\n'] = "\\n";
+    REPLACEMENT_CHARS['\r'] = "\\r";
+    REPLACEMENT_CHARS['\f'] = "\\f";
+  }
+
   private final boolean prettyPrint;
 
   /**
-   * Constructor.
+   * Construct a JsonSerializerImpl that does <b>not</b> pretty print the Payload.
    */
   public JsonSerializerImpl() {
     this(false);
   }
 
   /**
-   * Constructor.
-   *
-   * @param prettyPrint flag to pretty print the json.
+   * Construct a JsonSerializerImpl.
+   * @param prettyPrint whether or not to pretty print the payload.
    */
   public JsonSerializerImpl(boolean prettyPrint) {
     this.prettyPrint = prettyPrint;
   }
 
-  private static void serializeNumber(StringBuilder builder, Number value) {
-    builder.append(value);
-  }
-
-  private static void serializeBoolean(StringBuilder builder, Boolean value) {
-    builder.append(value ? "true" : "false");
-  }
-
-  private static void serializeNull(StringBuilder builder) {
-    builder.append("null");
-  }
-
-  private static void serializeThrowable(StringBuilder builder, Throwable value) {
-    final StringWriter writer = new StringWriter();
-    value.printStackTrace(new PrintWriter(writer));
-    builder.append(String.format("\"%s\"", writer.toString()));
-  }
-
-  private static void serializeDefault(StringBuilder builder, Object value) {
-    builder.append(String.format("\"%s\"", value));
-  }
-
-  private static Map<String, Object> asMap(Map value) {
-    try {
-      @SuppressWarnings("unchecked")
-      Map<String, Object> obj = (Map<String, Object>) value;
-      return obj;
-    } catch (ClassCastException e) {
-      Map<String, Object> obj = new LinkedHashMap<String, Object>();
-      for (Object o : value.entrySet()) {
-        Map.Entry thisOne = (Map.Entry) o;
-        Object key = thisOne.getKey();
-        Object val = thisOne.getValue();
-        obj.put(key.toString(), val);
-      }
-      return obj;
-    }
-  }
-
-  private static void serializeString(StringBuilder builder, String str) {
-    builder.append('"');
-    builder.append(str.replace("\"", "\\\""));
-    builder.append('"');
-  }
-
-  private static void indent(StringBuilder builder, int i) {
-    for (int x = 0; x <= i; x++) {
-      builder.append("  ");
-    }
-  }
-
   @Override
   public String toJson(Payload payload) {
-    return serialize(payload);
+    StringBuilder builder = new StringBuilder();
+    serializeValue(builder, payload, 0);
+    return builder.toString();
   }
 
   @Override
@@ -112,11 +78,6 @@ public class JsonSerializerImpl implements JsonSerializer {
         .build();
   }
 
-  private String serialize(JsonSerializable object) {
-    StringBuilder builder = new StringBuilder();
-    serializeValue(builder, object, 0);
-    return builder.toString();
-  }
 
   private void serializeObject(Map<String, Object> content, StringBuilder builder, int level) {
     builder.append('{');
@@ -171,6 +132,28 @@ public class JsonSerializerImpl implements JsonSerializer {
     }
   }
 
+  private static void serializeThrowable(StringBuilder builder, Throwable value) {
+    final StringWriter writer = new StringWriter();
+    value.printStackTrace(new PrintWriter(writer));
+    builder.append(String.format("\"%s\"", writer.toString()));
+  }
+
+  private static void serializeDefault(StringBuilder builder, Object value) {
+    builder.append(String.format("\"%s\"", value));
+  }
+
+  private static void serializeNumber(StringBuilder builder, Number value) {
+    builder.append(value);
+  }
+
+  private static void serializeBoolean(StringBuilder builder, Boolean value) {
+    builder.append(value ? "true" : "false");
+  }
+
+  private static void serializeNull(StringBuilder builder) {
+    builder.append("null");
+  }
+
   private void serializeArray(StringBuilder builder, Object[] array, int level) {
     builder.append('[');
     String comma = "";
@@ -187,5 +170,60 @@ public class JsonSerializerImpl implements JsonSerializer {
     builder.append(']');
   }
 
+  private static Map<String, Object> asMap(Map value) {
+    try {
+      @SuppressWarnings("unchecked")
+      Map<String, Object> obj = (Map<String, Object>) value;
+      return obj;
+    } catch (ClassCastException e) {
+      Map<String, Object> obj = new LinkedHashMap<>();
+      for (Object o : value.entrySet()) {
+        Map.Entry thisOne = (Map.Entry) o;
+        Object key = thisOne.getKey();
+        Object val = thisOne.getValue();
+        obj.put(key.toString(), val);
+      }
+      return obj;
+    }
+  }
+
+  // Borrowed from
+  // https://github.com/google/gson/blob/59edfc1caf2bb30e30f523f8502f23e8f8edc38e/gson/src/main/java/com/google/gson/stream/JsonWriter.java
+  private static void serializeString(StringBuilder builder, String str) {
+    builder.append('"');
+    int last = 0;
+    int length = str.length();
+    for (int i = 0; i < length; i++) {
+      char c = str.charAt(i);
+      String replacement;
+      if (c < 128) {
+        replacement = REPLACEMENT_CHARS[c];
+        if (replacement == null) {
+          continue;
+        }
+      } else if (c == '\u2028') {
+        replacement = "\\u2028";
+      } else if (c == '\u2029') {
+        replacement = "\\u2029";
+      } else {
+        continue;
+      }
+      if (last < i) {
+        builder.append(str, last, i);
+      }
+      builder.append(replacement);
+      last = i + 1;
+    }
+    if (last < length) {
+      builder.append(str, last, length);
+    }
+    builder.append('"');
+  }
+
+  private static void indent(StringBuilder builder, int i) {
+    for (int x = 0; x <= i; x++) {
+      builder.append("  ");
+    }
+  }
 
 }
