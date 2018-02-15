0cc0199497155b841c060461530b78de608c7c66
Andrew Weiss <wvvwwvw@gmail.com>
2018-02-15 11:30:22 -0800

    Allow extra metadata with servers

--- rollbar-api/src/test/java/com/rollbar/api/payload/data/ServerTest.java
+++ rollbar-api/src/test/java/com/rollbar/api/payload/data/ServerTest.java
@@ -39,4 +39,36 @@ public class ServerTest {
 
     assertThat(server.asJson(), is(expected));
   }
+
+  @Test
+  public void shouldAllowMetadata() {
+    Server baseServer = server();
+    Map<String, Object> metadataMap = new HashMap<>();
+    metadataMap.put("a", "b");
+    metadataMap.put("num", 42);
+    metadataMap.put("host", "should not show up");
+
+    Server server = new Server.Builder(baseServer)
+      .metadata(metadataMap)
+      .build();
+
+    Map<String, Object> expected = new HashMap<>();
+    expected.put("a", "b");
+    expected.put("num", 42);
+
+    if (server.getHost() != null) {
+      expected.put("host", server.getHost());
+    }
+    if (server.getRoot() != null) {
+      expected.put("root", server.getRoot());
+    }
+    if (server.getBranch() != null) {
+      expected.put("branch", server.getBranch());
+    }
+    if (server.getCodeVersion() != null) {
+      expected.put("code_version", server.getCodeVersion());
+    }
+
+    assertThat(server.asJson(), is(expected));
+  }
 }
