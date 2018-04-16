95f534521965476e3e39823068f0204f8bb5004d
jsjem <kirill.g@attendify.com>
2018-04-16 18:50:15 +0300

    reformat code for ObjectsUtils

--- rollbar-java/src/main/java/com/rollbar/notifier/util/ObjectsUtils.java
+++ rollbar-java/src/main/java/com/rollbar/notifier/util/ObjectsUtils.java
@@ -12,31 +12,31 @@ import java.util.Arrays;
  */
 public class ObjectsUtils {
 	
-	private static Logger logger = LoggerFactory.getLogger(ObjectsUtils.class);
+  private static Logger logger = LoggerFactory.getLogger(ObjectsUtils.class);
 	
-	public static boolean equals(Object object1, Object object2) {
-		return object1 == object2 || object1 != null && object1.equals(object2);
-	}
+  public static boolean equals(Object object1, Object object2) {
+    return object1 == object2 || object1 != null && object1.equals(object2);
+  }
 	
-	public static int hash(Object... objects) {
-		return Arrays.hashCode(objects);
-	}
+  public static int hash(Object... objects) {
+    return Arrays.hashCode(objects);
+  }
 	
-	public static <T> T requireNonNull(T object, String errorMessage) {
-		if (object == null) {
-			throw new NullPointerException(errorMessage);
-		} else {
-			return object;
-		}
-	}
+  public static <T> T requireNonNull(T object, String errorMessage) {
+    if (object == null) {
+      throw new NullPointerException(errorMessage);
+    } else {
+  	  return object;
+    }
+  }
 	
-	public static void close(final Closeable closeable) {
-		try {
-			if (closeable != null) {
-				closeable.close();
-			}
-		} catch (IOException e) {
-			logger.error("Unable to close stream.", e);
-		}
-	}
+  public static void close(final Closeable closeable) {
+  	 try {
+  	   if (closeable != null) {
+  	  	  closeable.close();
+  	   }
+  	 } catch (IOException e) {
+  	   logger.error("Unable to close stream.", e);
+  	 }
+  }
 }
