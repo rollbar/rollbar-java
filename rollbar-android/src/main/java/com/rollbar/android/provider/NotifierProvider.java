864af4c4a9caafc16cb9d10e30f02081393b1ab0
David Basoco <basoko@gmail.com>
2020-04-13 14:18:05 +0200

    Remove VERSION constant in rollbar-android and load from metadata
    A new metadata property has been added with the name
    "com.rollbar.notifier.version" in the library AndroidManifest
    that will be injected from gradle using manifest place holders.

--- rollbar-android/src/main/java/com/rollbar/android/provider/NotifierProvider.java
+++ rollbar-android/src/main/java/com/rollbar/android/provider/NotifierProvider.java
@@ -1,5 +1,12 @@
 package com.rollbar.android.provider;
 
+import static com.rollbar.android.util.Constants.ROLLBAR_NAMESPACE;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
 import com.rollbar.api.payload.data.Notifier;
 import com.rollbar.notifier.provider.Provider;
 
@@ -8,11 +15,19 @@ import com.rollbar.notifier.provider.Provider;
  */
 public class NotifierProvider implements Provider<Notifier> {
 
+    private static final String VERSION_METADATA_NAME = ROLLBAR_NAMESPACE + "._notifier.version";
+
+    private static final String NAME = "rollbar-android";
+
     private final Notifier notifier;
+
+    public NotifierProvider(Context context) {
+        this(loadVersionFromContext(context));
+    }
 
     public NotifierProvider(String version) {
         this.notifier = new Notifier.Builder()
-                .name("rollbar-android")
+                .name(NAME)
                 .version(version)
                 .build();
     }
@@ -27,5 +42,15 @@ public class NotifierProvider implements Provider<Notifier> {
     @Override
     public Notifier provide() {
         return notifier;
+    }
+
+    private static String loadVersionFromContext(Context context) {
+        try {
+            ApplicationInfo ai = context.getPackageManager().getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
+            Bundle data = ai.metaData;
+            return data.getString(VERSION_METADATA_NAME);
+        } catch(PackageManager.NameNotFoundException e) {
+            return "unknown";
+        }
     }
 }
