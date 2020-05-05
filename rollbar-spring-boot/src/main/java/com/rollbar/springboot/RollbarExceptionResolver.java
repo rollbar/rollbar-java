742e7ad79989b842edd48546c2e8827f71547a9e
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2020-05-05 07:51:55 -0700

    updates to POC

--- rollbar-spring-boot/src/main/java/com/rollbar/springboot/RollbarExceptionResolver.java
+++ rollbar-spring-boot/src/main/java/com/rollbar/springboot/RollbarExceptionResolver.java
@@ -1,6 +1,8 @@
 package com.rollbar.springboot;
 
 import com.rollbar.notifier.Rollbar;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,27 +10,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 public class RollbarExceptionResolver implements HandlerExceptionResolver, Ordered {
 
-    private Rollbar rollbar;
+  private Rollbar rollbar;
 
-    public RollbarExceptionResolver(Rollbar rollbar) {
-        this.rollbar = rollbar;
-    }
+  public RollbarExceptionResolver(Rollbar rollbar) {
+    this.rollbar = rollbar;
+  }
 
-    @Override
-    public ModelAndView resolveException(HttpServletRequest request,
-                                         HttpServletResponse response,
-                                         Object handler,
-                                         Exception ex) {
+  @Override
+  public ModelAndView resolveException(HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     Object handler,
+                                     Exception ex) {
+    System.out.println("******************* inside RollbarExceptionResolver *******************");
+    rollbar.error(ex);
 
-        rollbar.error(ex);
+    // null = run other HandlerExceptionResolvers to actually handle the exception
+    return null;
+  }
 
-        // null = run other HandlerExceptionResolvers to actually handle the exception
-        return null;
-    }
-
-    @Override
-    public int getOrder() {
-        // ensure this resolver runs first so that all exceptions are reported
-        return Integer.MIN_VALUE;
-    }
+  @Override
+  public int getOrder() {
+    // ensure this resolver runs first so that all exceptions are reported
+    return Integer.MIN_VALUE;
+  }
 }
