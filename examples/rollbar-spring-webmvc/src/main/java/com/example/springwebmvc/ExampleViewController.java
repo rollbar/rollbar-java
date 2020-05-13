0ee72b666c838a87e0f591ed90d2bba03ef39abc
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2020-05-13 16:07:26 -0700

    adding more examples

--- examples/rollbar-spring-webmvc/src/main/java/com/example/springwebmvc/ExampleViewController.java
+++ examples/rollbar-spring-webmvc/src/main/java/com/example/springwebmvc/ExampleViewController.java
@@ -14,15 +14,26 @@ public class ExampleViewController {
 
   @RequestMapping("/")
   public void exceptionTest() {
-
     // This exception will be passed now via the exception resolver
     int x = 1 / 0;
+  }
 
+  @RequestMapping("/handledExceptionTest")
+  public void handledExceptionTest() {
+    // catch and send the exception back using Rollbar
+    try {
+      int x = 1 / 0;
+    } catch(Exception e) {
+      Rollbar rollbar = (Rollbar)context.getBean(Rollbar.class);
+      rollbar.log("log some error to Rollbar");
+
+      // continue to raise it and Rollbar will send the full payload
+      throw e;
+    }
   }
 
   @RequestMapping("/rollbarTest")
   public void rollbarTest() {
-
     // an example of sending the error back using the Rollbar object
     Rollbar rollbar = (Rollbar)context.getBean(Rollbar.class);
     rollbar.error("Error");
