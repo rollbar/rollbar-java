69a8e0832608d369c8fea3cff50ec3e117318fc4
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2020-05-14 20:24:50 -0700

    using app properties

--- examples/rollbar-spring-webmvc/src/main/java/com/example/config/RollbarConfig.java
+++ examples/rollbar-spring-webmvc/src/main/java/com/example/config/RollbarConfig.java
@@ -5,6 +5,7 @@ import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 import com.rollbar.notifier.Rollbar;
 import com.rollbar.spring.webmvc.RollbarExceptionResolver;
 import com.rollbar.web.provider.RequestProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +15,23 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 @ComponentScan({"com.example.springbootwebmvc","com.rollbar.spring.webmvc"})
 public class RollbarConfig {
 
+    @Value("${rollbar.access_token}")
+    private String accessToken;
+
+    @Value("${rollbar.environment}")
+    private String environment;
+
+    @Value("${rollbar.framework}")
+    private String framework;
+
     /**
      * Register a Rollbar bean to configure App with Rollbar.
      */
     @Bean(name = "rollbar")
     public Rollbar rollbar() {
-        return Rollbar.init(withAccessToken("<TOKEN>")
+        return Rollbar.init(withAccessToken(accessToken)
+                .environment(environment)
+                .framework(framework)
                 .request(new RequestProvider.Builder().build())
                 .build());
     }
