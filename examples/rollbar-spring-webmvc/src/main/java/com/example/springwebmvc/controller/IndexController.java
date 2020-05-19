b77a71fcdb364d7a748b1368e696cb809e0f6f27
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2020-05-19 05:31:04 -0700

    fixing lint issues

--- examples/rollbar-spring-webmvc/src/main/java/com/example/springwebmvc/controller/IndexController.java
+++ examples/rollbar-spring-webmvc/src/main/java/com/example/springwebmvc/controller/IndexController.java
@@ -11,45 +11,45 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 class IndexController {
 
-    @Autowired
-    private ApplicationContext context;
+  @Autowired
+  private ApplicationContext context;
 
-    /**
-     * Example for calling rollbar.log.
-     */
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    @ResponseBody
-    public String index() {
-        Rollbar rollbar = (Rollbar)context.getBean(Rollbar.class);
-        System.out.println(rollbar);
-        rollbar.log("log some error to Rollbar");
-        return "index";
-    }
+  /**
+   * Example for calling rollbar.log.
+   */
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  @ResponseBody
+  public String index() {
+    Rollbar rollbar = (Rollbar)context.getBean(Rollbar.class);
+    System.out.println(rollbar);
+    rollbar.log("log some error to Rollbar");
+    return "index";
+  }
 
-    /**
-     * Example of a caught exception and then re-raised for Rollbar to process it.
-     */
-    @RequestMapping(value = "/handledException", method = RequestMethod.GET)
-    @ResponseBody
-    public String testHandledException() {
-        try {
-            int x = 1 / 0;
-        } catch (Exception e) {
-            Rollbar rollbar = (Rollbar)context.getBean(Rollbar.class);
-            rollbar.log("caught an error");
-            throw e; // you can re-raise to for the Rollbar error handler to process it;
-        }
-        return "index";
+  /**
+   * Example of a caught exception and then re-raised for Rollbar to process it.
+   */
+  @RequestMapping(value = "/handledException", method = RequestMethod.GET)
+  @ResponseBody
+  public String testHandledException() {
+    try {
+      int x = 1 / 0;
+    } catch (Exception e) {
+      Rollbar rollbar = (Rollbar)context.getBean(Rollbar.class);
+      rollbar.log("caught an error");
+      throw e; // you can re-raise to for the Rollbar error handler to process it;
     }
+    return "index";
+  }
 
-    /**
-     * Example of an unhandled exception and Rollbar still processing it.
-     */
-    @RequestMapping(value = "/exception", method = RequestMethod.GET)
-    @ResponseBody
-    public void exception() {
-        int x = 1 / 0;
-    }
+  /**
+   * Example of an unhandled exception and Rollbar still processing it.
+  */
+  @RequestMapping(value = "/exception", method = RequestMethod.GET)
+  @ResponseBody
+  public void exception() {
+    int x = 1 / 0;
+  }
 
 }
 
--- examples/rollbar-spring-webmvc/src/main/java/com/example/springwebmvc/initializer/AppInitializer.java
+++ examples/rollbar-spring-webmvc/src/main/java/com/example/springwebmvc/initializer/AppInitializer.java
@@ -1,27 +1,33 @@
 package com.example.springwebmvc.initializer;
 
 import com.example.springwebmvc.config.RollbarConfig;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+
+
 public class AppInitializer implements WebApplicationInitializer {
 
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(RollbarConfig.class);
-        ctx.setServletContext(servletContext);
-        ctx.refresh();
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+    ctx.register(RollbarConfig.class);
+    ctx.setServletContext(servletContext);
+    ctx.refresh();
 
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
+    ServletRegistration.Dynamic servlet =
+            servletContext.addServlet("dispatcher",
+                    new DispatcherServlet(ctx));
+    servlet.setLoadOnStartup(1);
+    servlet.addMapping("/");
 
-    }
+  }
 
 }
