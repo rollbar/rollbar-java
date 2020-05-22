cc61d4bfc8b66e67c92539e678b455806c35aad8
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2020-05-22 08:01:08 -0700

    fixing lint issues

--- examples/rollbar-spring-boot-webmvc/src/main/java/com/example/springbootwebmvc/ExampleApplication.java
+++ examples/rollbar-spring-boot-webmvc/src/main/java/com/example/springbootwebmvc/ExampleApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExampleApplication {
 
+  /**
+   * Starts the Spring boot application.
+   */
   public static void main(String[] args) {
 
     SpringApplication.run(ExampleApplication.class, args);
