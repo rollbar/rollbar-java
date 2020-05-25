93a9a9fd31edfb09da5064e11a57ca664741f79a
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2020-05-25 16:40:27 -0700

    fixing lint issues

--- rollbar-spring-boot-webmvc/src/main/java/com/rollbar/spring/boot/webmvc/RollbarSpringBootConfiguration.java
+++ rollbar-spring-boot-webmvc/src/main/java/com/rollbar/spring/boot/webmvc/RollbarSpringBootConfiguration.java
@@ -1,27 +1,31 @@
 package com.rollbar.spring.boot.webmvc;
 
 import com.rollbar.web.listener.RollbarRequestListener;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
 
 import javax.servlet.ServletContext;
+
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RollbarSpringBootConfiguration {
 
-    @Bean
-    public ServletContextInitializer rollbarRequestListenerInitializer() {
-        return new ServletContextInitializer() {
+  /**
+   * Configures RollbarListener when app starts up. This enriches HTTP request data
+   * with Rollbar exceptions. You can view this data from the Rollbar app.
+  */
+  @Bean
+  public ServletContextInitializer rollbarRequestListenerInitializer() {
+    return new ServletContextInitializer() {
 
-            @Override
-            public void onStartup(ServletContext container) {
-                // Attach the RollbarRequestListner to attribute HTTP Request data into the Exception object
-                // This will be visible in Rollbar
-                container.addListener(RollbarRequestListener.class);
-            }
-
-        };
-    }
+      @Override
+      public void onStartup(ServletContext container) {
+        // Attach the RollbarRequestListner to attribute HTTP Request data into the Exception object
+        // This will be visible in Rollbar
+        container.addListener(RollbarRequestListener.class);
+      }
+    };
+  }
 
 }
