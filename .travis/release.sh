a5454cc549a6004ec9b9596c90253c54febad46a
David Basoco <basoko@gmail.com>
2020-05-30 10:49:10 +0200

    Close and release staging sonatype repository if final build if not publish the SNAPSHOT version

--- .travis/release.sh
+++ .travis/release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+set -ev
+
 REPO="rollbar/rollbar-java"
 BRANCH="master"
-
-set -ev
+VERSION=`cat gradle.properties | awk -F= '/^VERSION_NAME/ { print $2 }'`
 
 if [[ "$TRAVIS_REPO_SLUG" != "$REPO" ]]; then
   echo "Skipping release: wrong repository. Expected '$REPO' but was '$TRAVIS_REPO_SLUG'."
@@ -11,8 +12,15 @@ elif [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "Skipping release. It was pull request."
 elif [[ "$TRAVIS_BRANCH" != "$BRANCH" ]]; then
   echo "Skipping release. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+elif [[ -z $VERSION ]]; then
+    echo "Skipping release. Version value not found."
 else
- echo "Doing release..."
-  ./gradlew -Dorg.gradle.internal.http.socketTimeout=300000 -Dorg.gradle.internal.http.connectionTimeout=300000 publish
+  if [[ $VERSION == *"SNAPSHOT"* ]]; then
+     echo "Doing SNAPSHOT release..."
+     ./gradlew -Dorg.gradle.internal.http.socketTimeout=300000 -Dorg.gradle.internal.http.connectionTimeout=300000 publishToSonatype
+  else
+     echo "Doing release..."
+     ./gradlew -Dorg.gradle.internal.http.socketTimeout=300000 -Dorg.gradle.internal.http.connectionTimeout=300000 publishToSonatype closeAndReleaseRepository
+  fi
   echo "Release done!"
 fi
