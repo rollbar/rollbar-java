fbe80b5c4d9f9cb7e0e58a2707d0b5a7a280df5a
David Basoco <basoko@gmail.com>
2020-06-03 13:34:34 +0200

    Use semver regex to validate version used and check if SNAPSHOT release

--- .travis/release.sh
+++ .travis/release.sh
@@ -2,6 +2,17 @@
 
 set -ev
 
+# https://github.com/fsaintjacques/semver-tool/blob/master/src/semver
+NAT='0|[1-9][0-9]*'
+ALPHANUM='[0-9]*[A-Za-z-][0-9A-Za-z-]*'
+IDENT="$NAT|$ALPHANUM"
+FIELD='[0-9A-Za-z-]+'
+SEMVER_REGEX="\
+^($NAT)\\.($NAT)\\.($NAT)\
+(\\-(${IDENT})(\\.(${IDENT}))*)?\
+(\\+${FIELD}(\\.${FIELD})*)?$"
+
+
 REPO="rollbar/rollbar-java"
 BRANCH="master"
 VERSION=`cat gradle.properties | awk -F= '/^VERSION_NAME/ { print $2 }'`
@@ -14,8 +25,10 @@ elif [[ "$TRAVIS_BRANCH" != "$BRANCH" ]]; then
   echo "Skipping release. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 elif [[ -z $VERSION ]]; then
   echo "Skipping release. Version value not found."
+elif ! [[ $VERSION =~ $SEMVER_REGEX ]]; then
+  echo "Skipping release. Bad version used."
 else
-  if [[ $VERSION == *"SNAPSHOT"* ]]; then
+  if [[ ${BASH_REMATCH[5]} == 'SNAPSHOT' ]]; then
     echo "Doing SNAPSHOT release..."
     ./gradlew -Dorg.gradle.internal.http.socketTimeout=300000 -Dorg.gradle.internal.http.connectionTimeout=300000 publishToSonatype
   else
