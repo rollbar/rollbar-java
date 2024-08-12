84c6deccbed9eeb65c0c53885508a888665f69ed
Csaba Ilonka <csaba.ilonka@rollbar.com>
2024-08-12 12:58:56 +0300

    Refresh signing key.

--- .github/scripts/temp-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export GPG_KEY_LOCATION="$(realpath "$GPG_KEY_LOCATION")"
-
-echo "Doing SNAPSHOT release..."
-
-./gradlew -Dorg.gradle.internal.http.socketTimeout=300000 -Dorg.gradle.internal.http.connectionTimeout=300000 publishToSonatype
-
-echo "Release done!"
