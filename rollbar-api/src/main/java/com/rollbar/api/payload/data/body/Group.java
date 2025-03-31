f191a898774bbc35189b9b6739180ee3225a72b0
chris <christian.buongarzoni@gmail.com>
2025-03-31 02:04:33 -0300

    chore: add Java doc and fix indentations

--- rollbar-api/src/main/java/com/rollbar/api/payload/data/body/Group.java
+++ rollbar-api/src/main/java/com/rollbar/api/payload/data/body/Group.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
 
+/**
+ * This represent a Group of trace chains in a Thread.
+ * In Java we only send 1 TraceChain per Group, in other languages like Python (ExceptionGroup) or
+ * JS (AggregatorError), it may be more TraceChains per group.
+ */
 public class Group implements JsonSerializable, StringTruncatable<Group> {
   private final BodyContent traceChain;
 
@@ -30,7 +35,9 @@ public class Group implements JsonSerializable, StringTruncatable<Group> {
 
   @Override
   public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) return false;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
     Group group = (Group) o;
     return Objects.equals(traceChain, group.traceChain);
   }
