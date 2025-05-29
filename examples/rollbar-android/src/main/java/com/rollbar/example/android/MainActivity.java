073a470c1de97994beaca31d0f4a2989d453816b
chris <christian.buongarzoni@gmail.com>
2025-05-29 03:12:11 -0300

    build(android-example): migrate old libs to AndroidX

--- examples/rollbar-android/src/main/java/com/rollbar/example/android/MainActivity.java
+++ examples/rollbar-android/src/main/java/com/rollbar/example/android/MainActivity.java
@@ -1,14 +1,16 @@
 package com.rollbar.example.android;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.rollbar.android.Rollbar;
 
 public class MainActivity extends AppCompatActivity {
