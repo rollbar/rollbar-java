8588ba9148d9d22092f99dffb4cb48d0e2049125
chris <christian.buongarzoni@gmail.com>
2025-08-04 01:31:05 -0300

    build: update android build.gradle to kotlin

---
+++ rollbar-android/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.library") version "8.6.0"
+    //id("com.rollbar.conventions.release") //TODO: AGP not applied, don't know why
+}
+
+apply(from = "$rootDir/gradle/android.quality.gradle") //TODO: Update as convention plugin
+
+android {
+    namespace = "com.rollbar.android"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+        consumerProguardFiles("proguard-rules.pro")
+        manifestPlaceholders["notifierVersion"] = project.property("VERSION_NAME") as String
+    }
+
+    buildTypes {
+        getByName("release") {
+            enableUnitTestCoverage = false
+            enableAndroidTestCoverage = false
+        }
+        getByName("debug") {
+            enableUnitTestCoverage = true
+            enableAndroidTestCoverage = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    api(project(":rollbar-java"))
+
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("org.mockito:mockito-core:2.28.2")
+    androidTestImplementation("org.mockito:mockito-android:+")
+}
