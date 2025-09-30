dda2f29d32bca413f920b228d910f5496e98420f
Christian Ricardo <57886390+buongarzoni@users.noreply.github.com>
2025-09-30 19:04:00 -0300

    [Feat][SDK- 538] Update android quality plugin (#349)
    * build: use android quality plugin
    
    * refactor: use convention plugin
    
    * refactor: update convention plugin
    
    * refactor: Exclude Jacoco instrumentation for Android Tests
    
    ---------
    
    Co-authored-by: chris <christian.buongarzoni@gmail.com>

---
+++ build-logic/conventions/src/main/kotlin/AndroidQualityPlugin.kt
@@ -0,0 +1,83 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+import com.github.spotbugs.snom.SpotBugsExtension
+import com.github.spotbugs.snom.SpotBugsTask
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.quality.Checkstyle
+import org.gradle.api.plugins.quality.CheckstyleExtension
+import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
+import org.gradle.testing.jacoco.tasks.JacocoReport
+
+class AndroidQualityPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+
+            pluginManager.apply("checkstyle")
+            pluginManager.apply("com.github.spotbugs-base")
+            pluginManager.apply("jacoco")
+
+            extensions.configure(CheckstyleExtension::class.java) {
+                toolVersion = "8.2"
+                configFile = rootProject.file("tools/checkstyle/google_checks.xml")
+            }
+
+            tasks.register("checkstyleMain", Checkstyle::class.java) {
+                source = fileTree("src/main/java")
+                include("**/*.java")
+                exclude("**/gen/**", "**/*Test.java", "**/annotation/*.java")
+                classpath = files()
+                reports {
+                    xml.required.set(false)
+                    html.required.set(true)
+                }
+                isIgnoreFailures = true
+            }
+
+            extensions.configure(SpotBugsExtension::class.java) {
+                toolVersion.set("6.3.0")
+                includeFilter.set(rootProject.file("tools/findbugs/findbugs.xml"))
+            }
+
+            tasks.register("spotbugsMain", SpotBugsTask::class.java) {
+                dependsOn(tasks.matching { it.name.startsWith("assemble") })
+
+                ignoreFailures = true
+                effort.set(Effort.MAX)
+                reportLevel.set(Confidence.MEDIUM)
+
+                classes = fileTree("build/intermediates/javac/debug/classes/")
+                sourceDirs.from(fileTree("src/main/java"))
+                auxClassPaths.from(files())
+
+                projectName.set(project.name)
+                release.set(project.version.toString())
+
+                reports.create("html") {
+                    required.set(true)
+                    outputLocation.set(layout.buildDirectory.file("reports/spotbugs/main/spotbugs.html"))
+                    setStylesheet("fancy-hist.xsl")
+                }
+            }
+
+            extensions.configure(JacocoPluginExtension::class.java) {
+                toolVersion = "0.8.6"
+            }
+
+            tasks.register("jacocoTestReport", JacocoReport::class.java) {
+                dependsOn("test")
+
+                reports {
+                    xml.required.set(false)
+                    csv.required.set(false)
+                    html.required.set(true)
+                    html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+                }
+            }
+
+            tasks.named("check") {
+                dependsOn("checkstyleMain", "spotbugsMain")
+            }
+        }
+    }
+}
