ed9af2002287216ccecfa228a43b6593ebb45222
buongarzoni <christian.buongarzoni@gmail.com>
2026-03-24 16:15:43 -0300

    chore(okhttp): fix lint

--- rollbar-okhttp/src/main/java/com/rollbar/okhttp/NetworkTelemetryRecorder.java
+++ rollbar-okhttp/src/main/java/com/rollbar/okhttp/NetworkTelemetryRecorder.java
@@ -3,6 +3,7 @@ package com.rollbar.okhttp;
 import com.rollbar.api.payload.data.Level;
 
 public interface NetworkTelemetryRecorder {
-    void recordNetworkEvent(Level level, String method, String url, String statusCode);
-    void recordErrorEvent(Exception exception);
+  void recordNetworkEvent(Level level, String method, String url, String statusCode);
+
+  void recordErrorEvent(Exception exception);
 }
--- rollbar-okhttp/src/main/java/com/rollbar/okhttp/RollbarOkHttpInterceptor.java
+++ rollbar-okhttp/src/main/java/com/rollbar/okhttp/RollbarOkHttpInterceptor.java
@@ -1,39 +1,44 @@
 package com.rollbar.okhttp;
 
 import com.rollbar.api.payload.data.Level;
+
+import java.io.IOException;
+
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.io.IOException;
-
 public class RollbarOkHttpInterceptor implements Interceptor {
 
-    private final NetworkTelemetryRecorder recorder;
+  private final NetworkTelemetryRecorder recorder;
 
-    public RollbarOkHttpInterceptor(NetworkTelemetryRecorder recorder) {
-        this.recorder = recorder;
+  public RollbarOkHttpInterceptor(NetworkTelemetryRecorder recorder) {
+    this.recorder = recorder;
+  }
+
+  @Override
+  public Response intercept(Chain chain) throws IOException {
+    Request request = chain.request();
+
+    try {
+      Response response = chain.proceed(request);
+
+      if (response.code() >= 400 && recorder != null) {
+        recorder.recordNetworkEvent(
+            Level.CRITICAL,
+            request.method(),
+            request.url().toString(),
+            String.valueOf(response.code()));
+      }
+
+      return response;
+
+    } catch (IOException e) {
+      if (recorder != null) {
+        recorder.recordErrorEvent(e);
+      }
+
+      throw e;
     }
-
-    @Override
-    public Response intercept(Chain chain) throws IOException {
-        Request request = chain.request();
-
-        try {
-            Response response = chain.proceed(request);
-
-            if (response.code() >= 400 && recorder != null) {
-                recorder.recordNetworkEvent(Level.CRITICAL, request.method(), request.url().toString(), String.valueOf(response.code()));
-            }
-
-            return response;
-
-        } catch (IOException e) {
-            if (recorder != null) {
-                recorder.recordErrorEvent(e);
-            }
-
-            throw e;
-        }
-    }
+  }
 }
