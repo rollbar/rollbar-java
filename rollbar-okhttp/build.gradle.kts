3c4f505ace239d1fc9f79877866bdcb915d50c92
buongarzoni <christian.buongarzoni@gmail.com>
2026-05-01 17:02:22 -0300

    build(okhttp): remove redundant mockito-core declaration

--- rollbar-okhttp/build.gradle.kts
+++ rollbar-okhttp/build.gradle.kts
@@ -5,7 +5,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("com.squareup.okhttp3:mockwebserver:5.3.2")
-    testImplementation("org.mockito:mockito-core:5.23.0")
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
     api(project(":rollbar-api"))
     api("org.slf4j:slf4j-api:1.7.25")
